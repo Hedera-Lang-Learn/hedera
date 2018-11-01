22bd48a4ad0cd0798ac0ce473d7ec63783278fe7
James Tauber <jtauber@jtauber.com>
2018-11-01 11:55:43 -0400

    implemented basic backend system and databasetext backend

--- basic/settings.py
+++ basic/settings.py
@@ -219,3 +219,7 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT", "")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = True
+
+TEXT_PROVIDER_BACKENDS = [
+    "databasetext.backends.TextProviderBackend",
+]
