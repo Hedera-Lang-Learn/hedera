71a33c5bb0a608f919dcf4bcc9c71b252336d8a6
James Tauber <jtauber@jtauber.com>
2018-12-13 07:50:18 -0500

    more examples

--- lemmatization/lemmatizer.py
+++ lemmatization/lemmatizer.py
@@ -1,6 +1,8 @@
 from .models import lookup_form
 from .morpheus import morpheus
 
+from vocab_list.models import VocabularyList
+
 
 """
 Just test examples for now.
@@ -14,9 +16,7 @@ print("ἤρχοντο", lookup_form("ἤρχοντο"), morpheus("ἤρχοντ
 
 # ἤρχοντο {'ἄρχω', 'ἔρχομαι'} ['ἄρχω', 'ἔρχομαι']
 
-text = """
-Καὶ τῇ ἡμέρᾳ τῇ τρίτῃ γάμος ἐγένετο ἐν Κανὰ τῆς Γαλιλαίας
-"""
+text = """Καὶ τῇ ἡμέρᾳ τῇ τρίτῃ γάμος ἐγένετο ἐν Κανὰ τῆς Γαλιλαίας"""
 
 for token in text.split():
     print(token, lookup_form(token), morpheus(token, "grc"))
@@ -33,4 +33,31 @@ for token in text.split():
 # τῆς {'ὁ'} ['ὁ']
 # Γαλιλαίας {'Γαλιλαία'} []
 
-# TODO: hook this up to the lemma lattices
+text = """ἐξῆλθον ἐκ τῆς πόλεως καὶ ἤρχοντο πρὸς αὐτόν"""
+
+for token in text.split():
+    print(token, lookup_form(token), morpheus(token, "grc"))
+
+# ἐξῆλθον {'ἐξέρχομαι'} ['ἐξέρχομαι']
+# ἐκ {'ἐκ'} ['ἐκ']
+# τῆς {'ὁ'} ['ὁ']
+# πόλεως {'πόλις'} ['πόλις']
+# καὶ {'καί'} ['καί']
+# ἤρχοντο {'ἔρχομαι', 'ἄρχω'} ['ἄρχω', 'ἔρχομαι']
+# πρὸς {'πρός'} ['πρός']
+# αὐτόν {'αὐτός'} ['αὐτός']
+
+gnt80 = VocabularyList.objects.get(pk=1)
+
+entry = gnt80.entries.get(lemma="ἄρχω")
+print(entry.lemma, entry.gloss)
+
+# ἄρχω I reign, rule
+
+entry = gnt80.entries.get(lemma="ἔρχομαι")
+print(entry.lemma, entry.gloss)
+
+# ἔρχομαι I come, go
+
+
+# TODO: hook all this up to the lemma lattices
