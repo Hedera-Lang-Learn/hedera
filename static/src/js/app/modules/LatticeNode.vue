916b13178f2c5a6ddeed67bdca1ea2d8929e9a44
Patrick Altman <paltman@gmail.com>
2019-02-14 08:03:01 -0600

    Break out the mark resolved link to own component

--- static/src/js/app/modules/LatticeNode.vue
+++ static/src/js/app/modules/LatticeNode.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="lattice-node">
+  <div class="lattice-node" v-if="node">
     <LatticeNode v-for="parent in node.parents" :key="parent.pk" :node="parent" @selected="n => $emit('selected', n)" />
     <div class="lattice-node--heading" @click.prevent="onClick">{{ node.pk }}. {{ node.label }}</div>
     <LatticeNode v-for="child in node.children" :key="child.pk" :node="child" @selected="n => $emit('selected', n)" />
   </div>
+  <div class="lattice-node" v-else>No Lemma</div>
 </template>
 <script>
 export default {
--- static/src/js/app/modules/LatticeTree.vue
+++ static/src/js/app/modules/LatticeTree.vue
@@ -1,44 +1,40 @@
 <template>
-    <div class="lattice-tree">
-        <h4>{{ token.token }}</h4>
-
-        <LatticeNode :node="node" @selected="onSelect" v-if="node" />
-        <div v-else>
-            No Lemma
-        </div>
-
-        <a href="" @click.prevent="markResolved(!token.resolved)">Mark {{ token.resolved ? 'Unresolved' : 'Resolved '}}</a>
-    </div>
+  <div class="lattice-tree">
+    <h4>{{ token.token }}</h4>
+    <LatticeNode :node="node" @selected="onSelect" />
+    <MarkResolved :resolved="token.resolved" @toggle="markResolved" />
+  </div>
 </template>
 <script>
-import LatticeNode from './LatticeNode.vue';
-import { UPDATE_TOKEN } from '../constants';
+import LatticeNode from "./LatticeNode.vue";
+import MarkResolved from './MarkResolved.vue';
+import { UPDATE_TOKEN } from "../constants";
 
 export default {
-    props: ['token', 'index', 'node'],
-    components: { LatticeNode },
-    computed: {
-        textId() {
-            return this.$store.state.textId;
-        }
-    },
-    methods: {
-        onSelect(node) {
-            this.$store.dispatch(UPDATE_TOKEN, {
-                id: this.textId,
-                tokenIndex: this.index,
-                nodeId: node.pk,
-                resolved: this.token.resolved,
-            });
-        },
-        markResolved(resolved) {
-            this.$store.dispatch(UPDATE_TOKEN, {
-                id: this.textId,
-                tokenIndex: this.index,
-                nodeId: this.node ? this.node.pk : null,
-                resolved: resolved,
-            });
-        }
+  props: ["token", "index", "node"],
+  components: { LatticeNode, MarkResolved },
+  computed: {
+    textId() {
+      return this.$store.state.textId;
     }
-}
+  },
+  methods: {
+    onSelect(node) {
+      this.$store.dispatch(UPDATE_TOKEN, {
+        id: this.textId,
+        tokenIndex: this.index,
+        nodeId: node.pk,
+        resolved: this.token.resolved
+      });
+    },
+    markResolved({ resolved }) {
+      this.$store.dispatch(UPDATE_TOKEN, {
+        id: this.textId,
+        tokenIndex: this.index,
+        nodeId: this.node ? this.node.pk : null,
+        resolved
+      });
+    }
+  }
+};
 </script>
