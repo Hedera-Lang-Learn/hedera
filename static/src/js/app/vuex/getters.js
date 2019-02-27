294a9f907f093358248592a1b4f5d86f072f8637
Patrick Altman <paltman@gmail.com>
2019-02-27 17:10:56 -0600

    Build calc on known vocab

--- static/src/js/app/vuex/getters.js
+++ static/src/js/app/vuex/getters.js
@@ -1,3 +1,7 @@
 export default {
-
+  knownVocab: (state) => {
+    const totalTokens = state.tokens.length;
+    const knownTokens = state.tokens.filter(t => t.inVocabList).length;
+    return knownTokens / totalTokens;
+  }
 };
