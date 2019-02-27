11bd6128a0c7a0d5489559c52501bd19ffdab90f
James Tauber <jtauber@jtauber.com>
2019-02-27 13:51:56 -0500

    refactored get_lattice_node into smaller functions

--- lattices/utils.py
+++ lattices/utils.py
@@ -11,6 +11,50 @@ def make_label(kind, value, context):
     return label
 
 
+def make_lemma(lemma, context=""):
+
+    node_for_lemma = LatticeNode.objects.create(label=make_label("lemma", lemma, context))
+    LemmaNode.objects.create(context=context, lemma=lemma, node=node_for_lemma)
+
+    return node_for_lemma
+
+
+def make_form(form, context=""):
+
+    node_for_form = LatticeNode.objects.create(label=make_label("form", form, context))
+    FormNode.objects.create(context=context, form=form, node=node_for_form)
+
+    return node_for_form
+
+
+def get_or_create_node_for_lemma(lemma, context):
+
+    lemma_node = LemmaNode.objects.filter(context=context, lemma=lemma).first()
+    if lemma_node:
+        return lemma_node.node
+    else:
+        return make_lemma(lemma, context)
+
+
+def get_or_create_node_for_form(form, context):
+
+    form_node = FormNode.objects.filter(context=context, form=form).first()
+    if form_node:
+        return form_node.node
+    else:
+        return make_form(form, context)
+
+
+def foo(form, lemmas, context):
+
+    node_for_form = get_or_create_node_for_form(form, context)
+
+    for lemma in lemmas:
+        node_for_form.children.add(get_or_create_node_for_lemma(lemma, context))
+
+    return node_for_form
+
+
 def get_lattice_node(lemmas, form=None, context=""):
     """
     get or create a LatticeNode from the given lemma(s) (and possibly form)
@@ -28,43 +72,10 @@ def get_lattice_node(lemmas, form=None, context=""):
         return
 
     elif len(lemmas) == 1:
-        lemma = lemmas[0]
-        lemma_node = LemmaNode.objects.filter(context=context, lemma=lemma).first()
-        if lemma_node:
-            return lemma_node.node
-        else:
-            node = LatticeNode.objects.create(label=make_label("lemma", lemma, context))
-            LemmaNode.objects.create(context=context, lemma=lemma, node=node)
-            return node
+        return get_or_create_node_for_lemma(lemmas[0], context)
 
     else:  # more than one lemma
         if form is None:
             raise ValueError("form cannot be None if more than one lemma")
 
-        form_node = FormNode.objects.filter(context=context, form=form).first()
-        if form_node:
-            node = form_node.node
-            for lemma in lemmas:
-                lemma_node = LemmaNode.objects.filter(context=context, lemma=lemma).first()
-                if lemma_node:
-                    node_for_lemma = lemma_node.node
-                    if node_for_lemma not in node.children.all():  # check descendants?
-                        node.children.add(node_for_lemma)
-                else:
-                    node_for_lemma = LatticeNode.objects.create(label=make_label("lemma", lemma, context))
-                    LemmaNode.objects.create(context=context, lemma=lemma, node=node_for_lemma)
-                    node.children.add(node_for_lemma)
-            return node
-        else:
-            node = LatticeNode.objects.create(label=make_label("form", form, context))
-            FormNode.objects.create(context=context, form=form, node=node)
-            for lemma in lemmas:
-                lemma_node = LemmaNode.objects.filter(context=context, lemma=lemma).first()
-                if lemma_node:
-                    node_for_lemma = lemma_node.node
-                    node.children.add(node_for_lemma)
-                else:
-                    node_for_lemma = LatticeNode.objects.create(label=make_label("lemma", lemma, context))
-                    LemmaNode.objects.create(context=context, lemma=lemma, node=node_for_lemma)
-                    node.children.add(node_for_lemma)
-            return node
+        return foo(form, lemmas, context)
