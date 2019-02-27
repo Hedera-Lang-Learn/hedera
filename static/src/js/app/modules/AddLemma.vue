e198ae373c816f47b82dc6d4c0596917ac2fe5db
Patrick Altman <paltman@gmail.com>
2019-02-27 13:14:13 -0600

    Clear input box after one has been added

--- static/src/js/app/modules/AddLemma.vue
+++ static/src/js/app/modules/AddLemma.vue
@@ -2,7 +2,7 @@
     <div class="input-group add-lemma">
         <input type="text" class="form-control" placeholder="Add Lemma" v-model="model.lemma" />
         <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button" @click.prevent="$emit('addLemma', { lemma: model.lemma })">Add</button>
+            <button class="btn btn-outline-secondary" type="button" @click.prevent="onClick">Add</button>
         </div>
     </div>
 </template>
@@ -13,6 +13,12 @@ export default {
       model: {
         lemma: '',
       }
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('addLemma', { lemma: this.model.lemma });
+      this.model.lemma = '';
     }
   }
 }
