7243cfd29db080e1579c554acb68d2f7b2f13dc3
Patrick Altman <paltman@gmail.com>
2019-02-28 12:48:14 -0600

    Show gloss and headword

--- static/src/js/app/modules/LatticeNode.vue
+++ static/src/js/app/modules/LatticeNode.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="lattice-node" v-if="node">
     <LatticeNode v-for="parent in parents" :key="parent.pk" :node="parent" @selected="n => $emit('selected', n)" />
-    <div class="lattice-node--heading" @click.prevent="onClick">{{ node.pk }}. {{ node.label }}</div>
+    <div class="lattice-node--heading" @click.prevent="onClick">
+      <div>{{ node.pk }}. {{ node.label }}</div>
+      <div class="vocab-entries" v-if="vocabEntries && vocabEntries.length > 0">
+        <div class="vocab-entry" v-for="(entry, index) in vocabEntries" :key="index">
+          <span class="headword">{{ entry.headword }}</span>
+          <span class="gloss">{{ entry.gloss }}</span>
+        </div>
+      </div>
+    </div>
     <LatticeNode v-for="child in children" :key="child.pk" :node="child" @selected="n => $emit('selected', n)" />
   </div>
 </template>
@@ -33,9 +41,25 @@ export default {
     children() {
       return this.node.children && this.node.children.filter(node => formFilter(node, this.selectedToken.token));
     },
+    vocabEntries() {
+      return this.node.vocabulary_entries;
+    },
     selectedToken() {
       return this.$store.state.selectedToken;
     }
   }
 }
 </script>
+
+<style lang="scss">
+  @import "../../../scss/config";
+  .vocab-entries {
+    padding-left: 2rem;
+    font-weight: 500;
+    font-size: 10pt;
+    .gloss {
+      color: $gray-600;
+    }
+  }
+</style>
+
