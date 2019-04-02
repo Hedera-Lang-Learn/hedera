4586b28211f1c638455a38c5ad157e96e0f506e5
Patrick Altman <paltman@gmail.com>
2019-04-02 11:43:28 -0500

    Show lemmatization progress

--- lemmatized_text/views.py
+++ lemmatized_text/views.py
@@ -8,7 +8,7 @@ from . import models
 
 
 def lemmatized_texts(request):
-    return render(request, "lemmatized_text/list.html", { "lemmatized_texts": models.LemmatizedText.objects.all() })
+    return render(request, "lemmatized_text/list.html", { "lemmatized_texts": models.LemmatizedText.objects.all().order_by("pk") })
 
 
 def create(request):
@@ -21,7 +21,7 @@ def create(request):
             cloned_from = get_object_or_404(models.LemmatizedText, pk=request.POST.get("cloned_from"))
         lt = models.LemmatizedText.objects.create(title=title, lang=lang, data={}, cloned_from=cloned_from)
         lt.lemmatize(text, lang)
-        return redirect(f"/lemmatized_text/{lt.pk}")
+        return redirect("lemmatized_texts_list")
     if request.GET.get("cloned_from"):
         cloned_from = get_object_or_404(models.LemmatizedText, pk=request.GET.get("cloned_from"))
     return render(request, "lemmatized_text/create.html", {"cloned_from": cloned_from})
@@ -33,6 +33,7 @@ def delete(request, pk):
         text.delete()
         return redirect("lemmatized_texts_list")
     return render(request, "lemmatized_text/delete.html", { "text": text })
+
 
 def text(request, pk):
     text = get_object_or_404(models.LemmatizedText, pk=pk)
