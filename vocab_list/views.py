02fd8080660fda9dba98b550f377f88282d2b19d
Patrick Altman <paltman@gmail.com>
2019-04-04 21:49:37 -0500

    Make a personal vocab detail page

--- vocab_list/views.py
+++ vocab_list/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import DetailView, ListView
 
-from .models import VocabularyList
+from .models import VocabularyList, PersonalVocabularyList
 
 
 class VocabularyListListView(ListView):
@@ -13,3 +13,14 @@ class VocabularyListDetailView(DetailView):
 
     template_name = "vocab_list/detail.html"
     model = VocabularyList
+
+
+class PersonalVocabListDetailView(DetailView):
+
+    template_name = "vocab_list/personal.html"
+    model = PersonalVocabularyList
+    slug_field = "lang"
+    slug_url_kwarg = "lang"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
