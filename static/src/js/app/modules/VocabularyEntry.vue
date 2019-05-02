d6c9e84a61d4d04cbdfa06802550fcb6119b1523
Patrick Altman <paltman@gmail.com>
2019-05-02 07:13:46 -0500

    Fix reveal gloss in teacher mode

--- static/src/js/app/modules/VocabularyEntry.vue
+++ static/src/js/app/modules/VocabularyEntry.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="vocab-entry">
         <span class="headword">{{ entry.headword }}</span>
-        <span class="gloss" v-if="showGloss">{{ entry.gloss }}</span>
+        <span class="gloss" v-if="showGloss || showEntry">{{ entry.gloss }}</span>
         <a href @click.prevent="showGloss = true" v-else>Reveal Gloss</a>
     </div>
 </template>
 
 <script>
 export default {
-  props: ['entry'],
+  props: ['entry', 'showEntry'],
   data() {
     return {
       showGloss: false,
