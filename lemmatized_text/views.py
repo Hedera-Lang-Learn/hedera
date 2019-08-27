7a045c2163543c1387758ba675a52ee391b4014f
Patrick Altman <paltman@gmail.com>
2019-08-27 11:00:59 -0500

    Make use of public/created_by

--- lemmatized_text/views.py
+++ lemmatized_text/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
 from . import models
@@ -5,7 +6,9 @@ from . import models
 
 def lemmatized_texts(request):
     return render(request, "lemmatized_text/list.html", {
-        "lemmatized_texts": models.LemmatizedText.objects.all().order_by("pk")
+        "lemmatized_texts": models.LemmatizedText.objects.filter(
+            Q(public=True) | Q(created_by=request.user)
+        ).order_by("pk")
     })
 
 
@@ -17,7 +20,13 @@ def create(request):
         text = request.POST.get("text")
         if request.POST.get("cloned_from"):
             cloned_from = get_object_or_404(models.LemmatizedText, pk=request.POST.get("cloned_from"))
-        lt = models.LemmatizedText.objects.create(title=title, lang=lang, data={}, cloned_from=cloned_from)
+        lt = models.LemmatizedText.objects.create(
+            title=title,
+            lang=lang,
+            data={},
+            cloned_from=cloned_from,
+            created_by=request.user
+        )
         lt.lemmatize(text, lang)
         return redirect("lemmatized_texts_list")
     if request.GET.get("cloned_from"):
@@ -26,18 +35,21 @@ def create(request):
 
 
 def delete(request, pk):
-    text = get_object_or_404(models.LemmatizedText, pk=pk)
+    text = get_object_or_404(models.LemmatizedText, pk=pk, created_by=request.user)
     if request.method == "POST":
         text.delete()
         return redirect("lemmatized_texts_list")
-    return render(request, "lemmatized_text/delete.html", { "text": text })
+    return render(request, "lemmatized_text/delete.html", {"text": text})
 
 
 def text(request, pk):
-    text = get_object_or_404(models.LemmatizedText, pk=pk)
-    return render(request, "lemmatized_text/text.html", { "text": text })
+    qs = models.LemmatizedText.filter(Q(public=True) | Q(created_by=request.user))
+    text = get_object_or_404(qs, pk=pk)
+    return render(request, "lemmatized_text/text.html", {"text": text})
 
 
 def learner_text(request, pk):
-    text = get_object_or_404(models.LemmatizedText, pk=pk)
-    return render(request, "lemmatized_text/learner_text.html", { "text": text })
+    qs = models.LemmatizedText.filter(Q(public=True) | Q(created_by=request.user))
+    text = get_object_or_404(qs, pk=pk)
+    return render(request, "lemmatized_text/learner_text.html", {"text": text})
+
