1d51bd0bbce803a50aa697f83077257d512c859d
James Tauber <jtauber@jtauber.com>
2019-11-05 08:24:19 -0500

    improved styling of lattice tree

--- static/src/js/app/modules/LatticeNode.vue
+++ static/src/js/app/modules/LatticeNode.vue
@@ -2,9 +2,13 @@
   <div class="lattice-node" v-if="node">
     <LatticeNode v-for="parent in parents" :key="parent.pk" :node="parent" @selected="n => $emit('selected', n)" />
     <div class="lattice-node--heading" @click.prevent="onClick">
-      <span class="lattice-id">{{ node.pk }}.</span>
-      <span class="lattice-label">{{ node.label }}</span>
-      <span class="lattice-gloss">{{ node.gloss }}</span>
+      <div>
+        <span class="lattice-id">{{ node.pk }}.</span>
+      </div>
+      <div>
+        <span class="lattice-label">{{ node.label }}</span>
+        <span class="lattice-gloss">{{ node.gloss }}</span>
+      </div>
     </div>
     <LatticeNode v-for="child in children" :key="child.pk" :node="child" @selected="n => $emit('selected', n)" />
   </div>
@@ -55,14 +59,21 @@ export default {
 
 <style lang="scss">
   @import "../../../scss/config";
+  .lattice-node--heading {
+    display: flex;
+  }
   .lattice-id {
-
+    padding-right: 0.5em;
+    font-size: 9pt;
   }
   .lattice-label {
-    font-weight: bold;
+    font-family: 'Noto Serif';
+    font-size: 13pt;
   }
   .lattice-gloss {
+    font-family: 'Noto Serif';
+    font-style: italic;
     color: $gray-600;
-
+    font-size: 11pt;
   }
 </style>
