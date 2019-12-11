46567ba843c095ed1f60402da336d8e7882a33a3
Patrick Altman <paltman@gmail.com>
2019-12-11 07:51:42 -0600

    Do not include tokens that won't be resolved in total count

--- static/src/js/app/vuex/getters.js
+++ static/src/js/app/vuex/getters.js
@@ -1,7 +1,7 @@
 export default {
   knownVocab: (state) => {
     const tokens = state.tokens.filter(t => t.word === t.word.toLowerCase());
-    const totalTokens = tokens.length;
+    const totalTokens = tokens.filter(t => t.resolved !== 'na').length;
     const knownTokens = tokens.filter(t => t.inVocabList).length;
     return knownTokens / totalTokens;
   },
