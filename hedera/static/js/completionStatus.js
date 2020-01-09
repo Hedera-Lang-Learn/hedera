6b4429eac22b6a09793f6d10903fd328dfa8433b
dodget <tylor_dodge@harvard.edu>
2020-01-09 16:32:30 -0500

    Fix template sting issue.

--- hedera/static/js/completionStatus.js
+++ hedera/static/js/completionStatus.js
@@ -9,36 +9,36 @@ const updatePage = (lemma, length) => {
   const lengthText = document.createTextNode(length);
   teacher.appendChild(teacherText);
   learner.appendChild(learnerText);
-  teacher.href = '/lemmatized_text/${lemma}/';
-  learner.href = '/lemmatized_text/${lemma}/learner/';
-  clone.href = '/lemmatized_text/create/?cloned_from=${lemma}';
+  teacher.href = `/lemmatized_text/${lemma}/`;
+  learner.href = `/lemmatized_text/${lemma}/learner/`;
+  clone.href = `/lemmatized_text/create/?cloned_from=${lemma}`;
   clone.classList.add('btn', 'btn-outline-primary', 'btn-sm');
   cloneIcon.classList.add('fa', 'fa-copy');
-  const title = document.getElementById('title-${lemma}');  
+  const title = document.getElementById(`title-${lemma}`);  
   title.appendChild(document.createElement('br'));
   title.appendChild(teacher);
   title.appendChild(document.createTextNode(' \u2022 '));
   title.appendChild(learner);
-  const progressDiv = document.getElementById('progress-${lemma}');
+  const progressDiv = document.getElementById(`progress-${lemma}`);
   const progressDivParent = progressDiv.parentNode;
   progressDivParent.removeChild(progressDiv);
   progressDivParent.appendChild(lengthText);
   clone.appendChild(cloneIcon);
   clone.appendChild(cloneText);
-  const buttonColumn = document.getElementById('button-column-${lemma}');
+  const buttonColumn = document.getElementById(`button-column-${lemma}`);
   buttonColumn.appendChild(clone);
 };
 
 
 const fetchStatus = (lemma) => {
-  window.fetch('${window.location.href}${lemma}/status/')
+  window.fetch(`${window.location.href}${lemma}/status/`)
     .then( (r) => {
       return r.json();
     })
     .then( (rj) => {
       const bar = document.getElementById(lemma);
       bar.setAttribute('aria-valuenow', rj.status);
-      bar.setAttribute('style', 'width: ${rj.status}%');
+      bar.setAttribute('style', `width: ${rj.status}%`);
       if(rj.status !== 100){
         setTimeout(fetchStatus, 5000, lemma);
       } else {
