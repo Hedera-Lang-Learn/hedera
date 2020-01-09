acd51c2ddbb8bb849a18e793aa98599dc4e33eca
dodget <tylor_dodge@harvard.edu>
2020-01-09 16:10:49 -0500

    Sort import.

--- lemmatized_text/views.py
+++ lemmatized_text/views.py
@@ -1,9 +1,9 @@
 import logging
 
 from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
-from django.http import JsonResponse
 
 from . import models
 
