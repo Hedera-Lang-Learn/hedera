73897f94dc99708e8417b48f1294cbbe6bbcb86d
Patrick Altman <paltman@gmail.com>
2020-06-19 16:27:03 -0500

    Use tabs for navigation

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -3,5 +3,8 @@ import '../scss/index.scss';
 import loadApps from './app';
 import handleMessageDismiss from './messages';
 
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/tab';
+
 loadApps();
 handleMessageDismiss();
