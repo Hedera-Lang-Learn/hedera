f2e20226bd6360305bf6d540ceefb1792c18bc65
Patrick Altman <paltman@gmail.com>
2020-06-22 11:37:52 -0500

    Add a copy button for join links

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -1,5 +1,7 @@
 import '../scss/index.scss';
 
+import Clipboard from 'clipboard';
+
 import loadApps from './app';
 import handleMessageDismiss from './messages';
 
@@ -8,3 +10,6 @@ import 'bootstrap/js/dist/tab';
 
 loadApps();
 handleMessageDismiss();
+
+// eslint-disable-next-line no-new
+new Clipboard('.btn-copy');
