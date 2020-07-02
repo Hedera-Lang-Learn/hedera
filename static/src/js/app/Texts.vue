5ef780bbd79e5376efce3786564adaaa459cd714
Patrick Altman <paltman@gmail.com>
2020-07-02 13:38:47 -0500

    Move add button to original texts list

--- static/src/js/app/Texts.vue
+++ static/src/js/app/Texts.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="lemmatized-texts mb-4">
     <div class="card">
-      <div class="card-header">
+      <div class="card-header d-flex justify-content-between">
         <ul class="nav nav-tabs card-header-tabs">
           <li class="nav-item" role="presentation">
-            <a class="nav-link" :class="{ active: selected === 'own-texts' }" href @click.prevent="selected = 'own-texts'">Original Texts</a>
+            <a class="nav-link" :class="{ active: ownSelected }" href @click.prevent="selected = 'own-texts'">Original Texts</a>
           </li>
           <li class="nav-item" role="presentation">
-            <a class="nav-link" :class="{ active: selected === 'class-texts' }" href @click.prevent="selected = 'class-texts'">Class Texts</a>
+            <a class="nav-link" :class="{ active: classSelected }" href @click.prevent="selected = 'class-texts'">Class Texts</a>
           </li>
         </ul>
+
+        <a v-if="ownSelected" class="btn btn-primary btn-sm" href="/lemmatized_text/create/"><i class="fa fa-plus"></i> Create</a>
       </div>
       <div class="tab-content">
         <div class="tab-pane fade show active">
@@ -43,15 +45,21 @@
       };
     },
     computed: {
+      ownSelected() {
+        return this.selected === 'own-texts';
+      },
+      classSelected() {
+        return this.selected === 'class-texts';
+      },
       activeTexts() {
-        return this.selected === 'own-texts' ? this.ownTexts : this.groupTexts;
+        return this.ownSelected ? this.ownTexts : this.groupTexts;
       },
       ownTexts() {
-        return this.texts.filter(text => text.clonedFor === null);
+        return this.texts.filter((text) => text.clonedFor === null);
       },
       groupTexts() {
-        return this.texts.filter(text => text.clonedFor !== null);
-      }
+        return this.texts.filter((text) => text.clonedFor !== null);
+      },
     },
     created() {
       api.fetchTexts((data) => {
