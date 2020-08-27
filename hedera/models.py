8c9b80e3a1cd799ee890ed37d375bf2dfb68add8
Patrick Altman <paltman@gmail.com>
2020-08-27 13:44:43 -0500

    Lints

--- hedera/models.py
+++ hedera/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=250, blank=True)
