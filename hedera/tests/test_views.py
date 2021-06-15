47834280a4677b97b0fa221dd7a250263731f158
Vesna Tan <tanvezi@gmail.com>
2021-06-15 11:29:31 -0400

    add test for redirect chain

--- hedera/tests/test_views.py
+++ hedera/tests/test_views.py
@@ -18,3 +18,8 @@ class DashboardViewTest(TestCase):
         self.assertEqual(dash_response.status_code, 200)
         self.assertContains(dash_response, "Dashboard")
         self.assertTemplateUsed(dash_response, "dashboard.html")
+    # docs for redirect_chain https://docs.djangoproject.com/en/3.2/topics/testing/tools/#making-requests
+    def test_login_redirect_to_dashboard(self):
+        dash_response = self.client.get('/account/dashboard/', follow=True)
+        self.assertEqual(dash_response.redirect_chain[0][0], "/account/login/?next=/account/dashboard/")
+        self.assertEqual(dash_response.redirect_chain[0][1], 302)
