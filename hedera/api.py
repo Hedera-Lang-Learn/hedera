49ef00e2c8269a4e64195fa45f6a5f513d39b1ef
Vesna Tan <tanvezi@gmail.com>
2021-07-26 16:08:25 -0400

    fix quotes and update precommit file

--- hedera/api.py
+++ hedera/api.py
@@ -319,7 +319,7 @@ class PersonalVocabularyQuickAddAPI(APIView):
 
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body)
-        data['node'] = get_object_or_404(LatticeNode, pk=data["node"])
+        data["node"] = get_object_or_404(LatticeNode, pk=data["node"])
         new_entry = PersonalVocabularyListEntry.objects.create(**data)
         return JsonResponse({"data": {"created": True, "data": new_entry.data()}})
 
