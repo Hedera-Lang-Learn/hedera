061db54047b7779bcd8bde9dff070d62d3ad7b97
Arthur Barrett <abarrett@fas.harvard.edu>
2022-05-17 15:51:10 -0400

    Updated models and added migration.

--- lemmatization/lemmatizer.py
+++ lemmatization/lemmatizer.py
@@ -1,9 +1,8 @@
 import logging
 
 from hedera.supported_languages import SUPPORTED_LANGUAGES
-from lattices.models import LatticeNode, LemmaNode
 
-from .models import add_form, lookup_form
+from .models import Lemma, lookup_form
 
 
 logger = logging.getLogger(__name__)
@@ -35,14 +34,13 @@ class Lemmatizer(object):
 
     def _lemmatize_token(self, token):
         s = lookup_form(token, self.lang)
-        if not s or self.force_refresh:
-            lemmas = self._service.lemmatize(token)
-            s |= add_form(
-                context=self._service.SID,
-                lang=self.lang,
-                form=token,
-                lemmas=lemmas
-            )
+        # if not s or self.force_refresh:
+        #     lemmas = self._service.lemmatize(token)
+        #     s |= add_form(
+        #         lang=self.lang,
+        #         form=token,
+        #         lemmas=lemmas
+        #     )
         return list(s)
 
     def _report_progress(self, index, total_count):
@@ -50,76 +48,33 @@ class Lemmatizer(object):
             self.cb((index + 1) / total_count)
 
     def lemmatize(self, text):
-        context = self._service.SID  # e.g. "morpheus"
         result = []
         tokens = list(self._tokenize(text))
         total_count = len(tokens)
         for index, token in enumerate(tokens):
             word, word_normalized, following = token
+            lemma_id = None
+            gloss_id = None
             if word_normalized:
                 lemmas = self._lemmatize_token(word_normalized)
+                lemma_entries = Lemma.objects.filter(lemma__in=lemmas).order_by("rank")
 
-                # node = get_lattice_node(lemmas, word)  # @@@ not sure what to use for context here
-
-                lemmas = sorted(lemma.rstrip("1") for lemma in lemmas)
-
-                label = " or ".join(lemmas)
-                lemma_node = LemmaNode.objects.filter(
-                    context=context,
-                    lemma=label).first()
-                logger.debug(f"lemmatize {context} -> {word} {lemmas} {label}")
-
-                if lemma_node:
-                    node = lemma_node.node
-                    logger.debug(f"got lemma node {lemma_node.pk} pointing to lattice node {node.pk}")
-                else:
-                    logger.debug("did not get lemma node")
-                    if len(lemmas) > 1:
-                        lattice_node = LatticeNode.objects.create(
-                            label=label,
-                            gloss=f"{context} ambiguity",
-                            canonical=False,
-                        )
-                        for lemma in lemmas:
-                            logger.debug(lemma)
-                            lemma_node = LemmaNode.objects.filter(
-                                context=context,
-                                lemma=lemma).first()
-                            logger.debug(lemma_node)
-                            if lemma_node:
-                                child_lattice_node = lemma_node.node
-                                lattice_node.children.add(child_lattice_node)
-                        lattice_node.save()
-                        lemma_node = LemmaNode.objects.create(
-                            context=context,
-                            lemma=label,
-                            node=lattice_node,
-                        )
-                        children = lattice_node.children.all()
-                        logger.debug(f"ambiguous so creating join node {lattice_node.pk} with children {children}")
-                        if len(children) == 1:
-                            node = children[0]
-                        else:
-                            node = lattice_node
-                    else:
-                        node = None
-
-                if node:
-                    if node.children.exists():
-                        resolved = RESOLVED_UNRESOLVED
-                    else:
-                        resolved = RESOLVED_NO_AMBIGUITY
-                else:
+                # TODO: do we still need resolution states?
+                if len(lemma_entries) == 0:
                     resolved = RESOLVED_NO_LEMMA
-            else:
-                node = None
-                resolved = RESOLVED_NA
-            node_pk = node.pk if node else None
+                if len(lemma_entries) == 1:
+                    resolved = RESOLVED_NO_AMBIGUITY
+                    lemma_id = lemma_entries[0].pk
+                elif len(lemma_entries) > 1:
+                    resolved = RESOLVED_AUTOMATIC
+                    lemma_id = lemma_entries[0].pk
+
             result.append(dict(
                 word=word,
                 word_normalized=word_normalized,
                 following=following,
-                node=node_pk,
+                lemma_id=lemma_id,
+                gloss_id=gloss_id,
                 resolved=resolved
             ))
             self._report_progress(index, total_count)
--- lemmatization/models.py
+++ lemmatization/models.py
@@ -1,22 +1,36 @@
+from typing import List
+
 from django.db import models
 
 from .utils import strip_macrons
 
 
+class Lemma(models.Model):
+    # TODO: consider indexing alt_lemma and rank
+    lang = models.CharField(max_length=3)  # ISO 639.2
+    lemma = models.CharField(max_length=255, blank=False)  # Hedera lemma (hic vs hīc)
+    alt_lemma = models.CharField(max_length=255, db_index=True)  # Morpheus lemma (hic1 vs hic2)
+    label = models.CharField(max_length=255)
+
+    # Lemma frequency data
+    rank = models.IntegerField(default=999999)  # Use this as the default sort. Lower rank = more frequent.
+    count = models.IntegerField(default=0)  # Higher count = more frequent
+    rate = models.FloatField(default=0)  # Higher rate = more frequent.
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["lemma", "lang"], name="unique_lemma_lang")
+        ]
+
+
 class FormToLemma(models.Model):
     """
-    mapping from surface form to lemma before any lattice consideration
-
-    This can be viewed as a cache of some external lemmatization process.
-
-    The "context" is just an optional label to clarify the source of the
-    lookup and should be interpreted the same as in the `lattices` app.
+    mapping from surface form to lemma
     """
 
-    context = models.CharField(max_length=255, blank=True)
     lang = models.CharField(max_length=3)  # ISO 639.2
     form = models.CharField(max_length=255)
-    lemma = models.CharField(max_length=255)
+    lemma = models.ForeignKey("Lemma", blank=False, on_delete=models.CASCADE, related_name="forms")
 
     # @@@ do we need created/updated? source?
 
@@ -37,16 +51,34 @@ class FormToLemma(models.Model):
                 lemma=strip_macrons(lemma),
             )
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["lang", "form", "lemma"], name="unique_lang_form_lemma")
+        ]
 
-def lookup_form(form, lang):
+
+class Gloss(models.Model):
+    # Note about efficiency:
+    #   Consider changing model to many-to-many with Lemma to be more space efficient.
+    #   For example: "a", "ab", "abs" each refer to two definitions, but they are the *same* definitions:
+    #       by (+abl.)
+    #       from,away (+abl.)
+    #
+    lemma = models.ForeignKey("Lemma", blank=False, on_delete=models.CASCADE, related_name="glosses")
+    gloss = models.TextField(max_length=1024)
+
+
+def lookup_form(form, lang) -> List[str]:
+    """ Returns a list of lemmas (strings) in frequency order. """
     entries = FormToLemma.objects.filter(form=form, lang=lang)
-    return set(entries.values_list("lemma", flat=True))
+    lemma_ids = set(entries.values_list("lemma", flat=True))
+    lemmas = Lemma.objects.filter(pk__in=lemma_ids).order_by("rank")
+    return list(lemmas.values_list("lemma", flat=True))
 
 
-def add_form(context, lang, form, lemmas):
+def add_form(lang, form, lemmas):
     for lemma in lemmas:
         FormToLemma.objects.create(
-            context=context,
             lang=lang,
             form=form,
             lemma=lemma,
