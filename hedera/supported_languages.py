b98a43d8f538fb9fcd269e92060b78225d33fdfe
Vesna Tan <tanvezi@gmail.com>
2022-09-02 10:09:19 -0400

    added comment for disabled languages

--- hedera/supported_languages.py
+++ hedera/supported_languages.py
@@ -1,19 +1,22 @@
 from collections import namedtuple
 
-from lemmatization.services.chinese import ChineseService, LACChineseTokenizer
-from lemmatization.services.greek import GreekService, GreekTokenizer
+# from lemmatization.services.chinese import ChineseService, LACChineseTokenizer
+# from lemmatization.services.greek import GreekService, GreekTokenizer
 from lemmatization.services.latin import EncliticTokenizer, LatinService
-from lemmatization.services.russian import ClancyService, RUSTokenizer
+
+
+# from lemmatization.services.russian import ClancyService, RUSTokenizer
 
 
 # using ISO 639.2 CODES
 
 SupportedLanguage = namedtuple("SupportedLanguage", ["code", "verbose_name", "service", "tokenizer"])
 
-
+#NOTE: disables language here for version 1.0
+#TODO: Add back languages after
 SUPPORTED_LANGUAGES = {
-    "grc": SupportedLanguage("grc", "Ancient Greek", GreekService(lang="grc"), GreekTokenizer(lang="grc")),
+    # "grc": SupportedLanguage("grc", "Ancient Greek", GreekService(lang="grc"), GreekTokenizer(lang="grc")),
     "lat": SupportedLanguage("lat", "Latin", LatinService(lang="lat"), EncliticTokenizer(lang="lat")),
-    "rus": SupportedLanguage("rus", "Russian", ClancyService(lang="rus"), RUSTokenizer(lang="rus")),
-    "zho": SupportedLanguage("zho", "Chinese", ChineseService(lang="zho"), LACChineseTokenizer(lang="zho"))
+    # "rus": SupportedLanguage("rus", "Russian", ClancyService(lang="rus"), RUSTokenizer(lang="rus")),
+    # "zho": SupportedLanguage("zho", "Chinese", ChineseService(lang="zho"), LACChineseTokenizer(lang="zho"))
 }
