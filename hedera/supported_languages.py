e806fdc78a80f5b4a57da6591f47fd48436a7345
Vesna Tan <tanvezi@gmail.com>
2022-09-07 10:50:39 -0400

    remove duplicate codes from merge conflict

--- hedera/supported_languages.py
+++ hedera/supported_languages.py
@@ -1,28 +1,27 @@
 from collections import namedtuple
 
-# from lemmatization.services.russian import ClancyService, RUSTokenizer
-from lemmatization.services.chinese import (
-    ChinesePreprocessor,
-    ChineseService,
-    LACChineseTokenizer
-)
-from lemmatization.services.greek import (
-    GreekPreprocessor,
-    GreekService,
-    GreekTokenizer
-)
-# from lemmatization.services.chinese import ChineseService, LACChineseTokenizer
-# from lemmatization.services.greek import GreekService, GreekTokenizer
+# from lemmatization.services.chinese import (
+#     ChinesePreprocessor,
+#     ChineseService,
+#     LACChineseTokenizer
+# )
+# from lemmatization.services.greek import (
+#     GreekPreprocessor,
+#     GreekService,
+#     GreekTokenizer
+# )
 from lemmatization.services.latin import (
     EncliticTokenizer,
     LatinPreprocessor,
     LatinService
 )
-from lemmatization.services.russian import (
-    ClancyService,
-    RUSPreprocessor,
-    RUSTokenizer
-)
+
+
+# from lemmatization.services.russian import (
+#     ClancyService,
+#     RUSPreprocessor,
+#     RUSTokenizer
+# )
 
 
 # using ISO 639.2 CODES
@@ -33,15 +32,7 @@ SupportedLanguage = namedtuple("SupportedLanguage", ["code", "verbose_name", "se
 #TODO: Add back languages after
 SUPPORTED_LANGUAGES = {
     # "grc": SupportedLanguage("grc", "Ancient Greek", GreekService(lang="grc"), GreekTokenizer(lang="grc")),
-    "lat": SupportedLanguage("lat", "Latin", LatinService(lang="lat"), EncliticTokenizer(lang="lat")),
+    "lat": SupportedLanguage("lat", "Latin", LatinService(lang="lat"), EncliticTokenizer(lang="lat"), LatinPreprocessor(lang="lat")),
     # "rus": SupportedLanguage("rus", "Russian", ClancyService(lang="rus"), RUSTokenizer(lang="rus")),
     # "zho": SupportedLanguage("zho", "Chinese", ChineseService(lang="zho"), LACChineseTokenizer(lang="zho"))
-}
-SupportedLanguage = namedtuple("SupportedLanguage", ["code", "verbose_name", "service", "tokenizer", "preprocessor"])
-
-SUPPORTED_LANGUAGES = {
-    "grc": SupportedLanguage("grc", "Ancient Greek", GreekService(lang="grc"), GreekTokenizer(lang="grc"), GreekPreprocessor(lang="grc")),
-    "lat": SupportedLanguage("lat", "Latin", LatinService(lang="lat"), EncliticTokenizer(lang="lat"), LatinPreprocessor(lang="lat")),
-    "rus": SupportedLanguage("rus", "Russian", ClancyService(lang="rus"), RUSTokenizer(lang="rus"), RUSPreprocessor(lang="rus")),
-    "zho": SupportedLanguage("zho", "Chinese", ChineseService(lang="zho"), LACChineseTokenizer(lang="zho"), ChinesePreprocessor(lang="zho"))
 }
