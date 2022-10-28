b4c71379bc622c2cb9c9a2e5c6715fbee7d200de
Jeremy Guillette <3651377+jaguillette@users.noreply.github.com>
2022-10-28 14:02:03 -0400

    improve explanations and errors

--- split_vocab_spreadsheet_by_chapter.py
+++ split_vocab_spreadsheet_by_chapter.py
@@ -12,12 +12,27 @@ except IndexError:
 
 df = pd.read_csv(filename, sep="\t")
 
+if "chapter" not in df.columns:
+    print("Spreadsheet must include a 'chapter' column")
+    exit()
+
+# Get unique chapters in a list as ints
 chapters = df.chapter.unique()
 chapters.sort()
 chapters = [int(chapter) for chapter in chapters]
+
+# Get the max number of digits in chapters, so they can be numbered like "01"
+# or "028" as needed
 digits = len(str(max(chapters)))
+
+# Make an output format template with the number of digits calculated above
 outputformat = f"{{filename_base}}_ch{{c:0{digits}}}.tsv"
 
+# Output a new spreadsheet for each chapter. The list should include the
+# chapter in the filename, as well as all lower-numbered chapters. The
+# filenames will be formatted like {filename_base}_ch{chapter_number}.tsv,
+# where chapter_numbers are left-padded with zeros up to the maximum number of
+# digits in the list of unique chapters.
 for c in chapters:
     df[df.chapter <= c].to_csv(
         outputformat.format(filename_base=filename_base, c=c),
