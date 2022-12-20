dab5584e20223133ce860f4f820eb60a4a6031f5
Vesna Tan <tanvezi@gmail.com>
2022-12-20 18:15:10 -0500

    Add initial to EditTextHtmlParser

--- lemmatized_text/parsers.py
+++ lemmatized_text/parsers.py
@@ -14,6 +14,7 @@ class EditedTextHtmlParser(HTMLParser):
         self.lemmatized_text_data = []
         self.token_lemma_dict = token_lemma_dict
         self.lemmatizer = Lemmatizer(lang)
+        self.initial = ""
         return super().__init__()
 
     def handle_starttag(self, tag, attrs):
@@ -32,6 +33,13 @@ class EditedTextHtmlParser(HTMLParser):
                 self.current_attrs = json.loads(value)
 
     def handle_endtag(self, tag):
+        # Add new initial if self.initial is set
+        if len(self.lemmatized_text_data) == 1 and self.initial:
+            lemmatized_text_data_initial = self.lemmatized_text_data[0]["initial"]
+            if lemmatized_text_data_initial != self.initial:
+                lemmatized_text_data_initial = self.initial
+                # Reset self.initial
+                self.initial = ""
         if "follower" in self.current_attrs:
             self.separate_true_followers(self.current_data)
         #Note: sometimes the current_tag/self.current_attrs will be empty/None when there is a newline/break
@@ -102,16 +110,23 @@ class EditedTextHtmlParser(HTMLParser):
         Checks if chunk does not contain return and newline "\r\n" - only add tokens if it the chunk is not a return/newline
         **Fixes problem with empty tokens**
         Returns None
-
-        Note: We are checking the length of self.lemmatized_text_data due to newlines/breaks that may happen at the very beginning of the text
         """
         self.current_data = None
+        # lemmatized_text_data_length = len(self.lemmatized_text_data)
         new_data = self.lemmatizer.lemmatize(chunk)
         if "\r\n" not in chunk:
             self.lemmatized_text_data.extend(new_data)
         elif "\r\n" in chunk and len(self.lemmatized_text_data):
             following = self.lemmatized_text_data[-1]["following"]
-            self.lemmatized_text_data[-1]["following"] = f"{following}{chunk}"
+            token_lemma_dict_keys = list(self.token_lemma_dict.keys())
+            prev_lemma_id = self.lemmatized_text_data[-1]["lemma_id"]
+            #Note: Added check if we have reached the end of the data array because theres a bug where new lines are added after each edit
+            if prev_lemma_id not in self.token_lemma_dict[token_lemma_dict_keys[-1]]:
+                self.lemmatized_text_data[-1]["following"] = f"{following}{chunk}"
+        # EDGE CASE: Newlines/breaks that may happen at the very beginning of the text
+        # Pull out beginning new lines and add to self.initial
+        else:
+            self.initial = chunk
 
 
 class TagStripper(HTMLParser):
