49b5cd9d84cee37085b2ac6f894da3a2037d5216
ashley-gong <ashley.q.gong@gmail.com>
2023-09-22 11:03:31 -0400

    added placeholder for read status in texts table

--- static/src/js/app/Texts.vue
+++ static/src/js/app/Texts.vue
@@ -4,19 +4,47 @@
       <div class="card-header d-flex justify-content-between">
         <ul class="nav nav-tabs card-header-tabs">
           <li class="nav-item" role="presentation">
-            <a class="nav-link" :class="{ active: ownSelected }" href @click.prevent="selected = 'own-texts'">Original Texts</a>
+            <a
+              class="nav-link"
+              :class="{ active: ownSelected }"
+              href
+              @click.prevent="selected = 'own-texts'"
+              >Original Texts</a
+            >
           </li>
           <li class="nav-item" role="presentation">
-            <a class="nav-link" :class="{ active: classSelected }" href @click.prevent="selected = 'class-texts'">Class Texts</a>
+            <a
+              class="nav-link"
+              :class="{ active: classSelected }"
+              href
+              @click.prevent="selected = 'class-texts'"
+              >Class Texts</a
+            >
           </li>
         </ul>
 
-        <a v-if="ownSelected" class="btn btn-primary btn-sm" href="/lemmatized_text/create/"><i class="fa fa-plus" aria-hidden="true"></i> Create</a>
+        <a
+          v-if="ownSelected"
+          class="btn btn-primary btn-sm"
+          href="/lemmatized_text/create/"
+          ><i class="fa fa-plus" aria-hidden="true"></i> Create</a
+        >
       </div>
       <div class="tab-content">
         <div class="tab-pane fade show active">
           <table class="table">
-            <tr><th>Text</th><th>Language</th><th>Length</th><th>Created At</th><th v-if="ownSelected" /><th v-else>Familiarity</th></tr>
+            <!-- ADD AS TABLE HEADER -> a READ/UNREAD column in Django as text attribute, button to toggle/automatic -->
+            <tr>
+              <th>Text</th>
+              <th>Language</th>
+              <th>Length</th>
+              <th>Created At</th>
+              <!-- <th v-if="ownSelected" /> -->
+              <!-- <th v-else>Read?</th> -->
+              <th>Read?</th>
+              <th v-if="ownSelected" />
+              <th v-else>Familiarity</th>
+            </tr>
             <TextRow
               v-for="text in activeTexts"
               :key="text.id"
@@ -31,42 +59,40 @@
 </template>
 
 <script>
-  import TextRow from './components/TextRow.vue';
-  import { PROFILE_FETCH, LEMMATIZED_TEXT_FETCH_LIST } from './constants';
+import TextRow from './components/TextRow.vue';
+import { PROFILE_FETCH, LEMMATIZED_TEXT_FETCH_LIST } from './constants';
 
-  export default {
-    components: {
-      TextRow,
+export default {
+  components: {
+    TextRow,
+  },
+  data() {
+    return {
+      selected: 'own-texts',
+    };
+  },
+  computed: {
+    ownSelected() {
+      return this.selected === 'own-texts';
     },
-    data() {
-      return {
-        selected: 'own-texts',
-      };
+    classSelected() {
+      return this.selected === 'class-texts';
     },
-    computed: {
-      ownSelected() {
-        return this.selected === 'own-texts';
-      },
-      classSelected() {
-        return this.selected === 'class-texts';
-      },
-      activeTexts() {
-        return this.ownSelected ? this.ownTexts : this.groupTexts;
-      },
-      ownTexts() {
-        return this.$store.state.texts.filter((text) => text.clonedFor === null);
-      },
-      groupTexts() {
-        return this.$store.state.texts.filter((text) => text.clonedFor !== null);
-      },
+    activeTexts() {
+      return this.ownSelected ? this.ownTexts : this.groupTexts;
     },
-    created() {
-      this.$store.dispatch(PROFILE_FETCH);
-      this.$store.dispatch(LEMMATIZED_TEXT_FETCH_LIST);
+    ownTexts() {
+      return this.$store.state.texts.filter((text) => text.clonedFor === null);
     },
-  };
+    groupTexts() {
+      return this.$store.state.texts.filter((text) => text.clonedFor !== null);
+    },
+  },
+  created() {
+    this.$store.dispatch(PROFILE_FETCH);
+    this.$store.dispatch(LEMMATIZED_TEXT_FETCH_LIST);
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- static/src/js/app/components/TextRow.vue
+++ static/src/js/app/components/TextRow.vue
@@ -9,103 +9,145 @@
       <span v-if="completed === 100">{{ tokenCount }}</span>
       <div v-else class="progress-container">
         <div class="progress">
-          <div class="progress-bar" :class="{'bg-warning': completed < 100}" role="progressbar" :style="`width: ${completed}%`" :aria-valuenow="completed" aria-valuemin="0" aria-valuemax="100" />
+          <div
+            class="progress-bar"
+            :class="{ 'bg-warning': completed < 100 }"
+            role="progressbar"
+            :style="`width: ${completed}%`"
+            :aria-valuenow="completed"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          />
         </div>
         <div class="status" v-if="teacherMode">
-          <small><a v-if="text.canRetry" href @click.prevent="onRetry">Retry</a></smalL>
-          <small><a v-if="text.canCancel" href @click.prevent="onCancel">Cancel</a></smalL>
+          <small
+            ><a v-if="text.canRetry" href @click.prevent="onRetry"
+              >Retry</a
+            ></small
+          >
+          <small
+            ><a v-if="text.canCancel" href @click.prevent="onCancel"
+              >Cancel</a
+            ></small
+          >
         </div>
       </div>
     </td>
-    <td>{{ this.$options.filters.dateFormat(this.$data.createdAt, 'MMM D, YYYY') }}</td>
+    <td>
+      {{
+        this.$options.filters.dateFormat(this.$data.createdAt, 'MMM D, YYYY')
+      }}
+    </td>
+    <td>Unread</td>
+    <!-- <td v-else>Unread</td> -->
     <td v-if="teacherMode">
-      <a class="btn btn-outline-danger btn-sm" :href="text.deleteUrl"><i class="fa fa-trash" aria-hidden="true" /> Delete</a>
-      <a v-if="completed == 100" class="btn btn-outline-primary btn-sm" :href="text.cloneUrl"><i class="fa fa-copy" aria-hidden="true" /> Clone</a>
-      <a v-if="completed == 100" class="btn btn-outline-info btn-sm" :href="text.editUrl"><i class="fa fa-edit" aria-hidden="true" /> Edit</a>
+      <a class="btn btn-outline-danger btn-sm" :href="text.deleteUrl"
+        ><i class="fa fa-trash" aria-hidden="true" /> Delete</a
+      >
+      <a
+        v-if="completed == 100"
+        class="btn btn-outline-primary btn-sm"
+        :href="text.cloneUrl"
+        ><i class="fa fa-copy" aria-hidden="true" /> Clone</a
+      >
+      <a
+        v-if="completed == 100"
+        class="btn btn-outline-info btn-sm"
+        :href="text.editUrl"
+        ><i class="fa fa-edit" aria-hidden="true" /> Edit</a
+      >
     </td>
     <td v-else>
       <div v-if="text.stats" class="text-familiarity mb-0">
-          <div class="familiarity-null">{{ text.stats.unranked }}</div>
-          <div class="familiarity-1">{{ text.stats.one }}</div>
-          <div class="familiarity-2">{{ text.stats.two }}</div>
-          <div class="familiarity-3">{{ text.stats.three }}</div>
-          <div class="familiarity-4">{{ text.stats.four }}</div>
-          <div class="familiarity-5">{{ text.stats.five }}</div>
+        <div class="familiarity-null">{{ text.stats.unranked }}</div>
+        <div class="familiarity-1">{{ text.stats.one }}</div>
+        <div class="familiarity-2">{{ text.stats.two }}</div>
+        <div class="familiarity-3">{{ text.stats.three }}</div>
+        <div class="familiarity-4">{{ text.stats.four }}</div>
+        <div class="familiarity-5">{{ text.stats.five }}</div>
       </div>
     </td>
   </tr>
 </template>
 
 <script>
-  import api from '../api';
+import api from '../api';
 
-  export default {
-    props: {
-      text: {
-        type: Object,
-        required: true,
-      },
-      teacherMode: {
-        type: Boolean,
-        default: false,
-      },
+export default {
+  props: {
+    text: {
+      type: Object,
+      required: true,
     },
-    computed: {
-      textUrl() {
-        return this.teacherMode ? `/lemmatized_text/${this.text.id}/` : `/lemmatized_text/${this.text.id}/learner/`;
-      },
+    teacherMode: {
+      type: Boolean,
+      default: false,
     },
-    data() {
-      return {
-        completed: this.text.completed,
-        createdAt: new Date(this.text.createdAt),
-        tokenCount: this.text.tokenCount,
-        lemmatizationStatus: this.text.lemmatizationStatus,
-      };
+  },
+  computed: {
+    textUrl() {
+      return this.teacherMode
+        ? `/lemmatized_text/${this.text.id}/`
+        : `/lemmatized_text/${this.text.id}/learner/`;
     },
-    created() {
-      if (this.text.completed < 100) {
-        this.updateStatus();
+  },
+  data() {
+    return {
+      completed: this.text.completed,
+      createdAt: new Date(this.text.createdAt),
+      tokenCount: this.text.tokenCount,
+      lemmatizationStatus: this.text.lemmatizationStatus,
+      public: this.text.public,
+    };
+  },
+  created() {
+    if (this.text.completed < 100) {
+      this.updateStatus();
+    }
+  },
+  methods: {
+    // can you update read status here? maybe 3 options - read, in progress, unread
+    async updateStatus() {
+      const response = await api.lemmatizedText_fetchStatus(this.text.id);
+      const { data } = response.data;
+      const { completed, tokenCount, lemmatizationStatus } = data;
+      this.completed = completed;
+      this.tokenCount = tokenCount;
+      this.lemmatizationStatus = lemmatizationStatus;
+      if (
+        this.completed < 100 &&
+        ['finished', 'failed', 'queued'].indexOf(this.lemmatizationStatus) ===
+          -1
+      ) {
+        setTimeout(this.updateStatus, 1000);
       }
     },
-    methods: {
-      async updateStatus() {
-        const response = await api.lemmatizedText_fetchStatus(this.text.id);
-        const { data } = response.data;
-        const { completed, tokenCount, lemmatizationStatus } = data;
-        this.completed = completed;
-        this.tokenCount = tokenCount;
-        this.lemmatizationStatus = lemmatizationStatus;
-        if (this.completed < 100 && ['finished', 'failed', 'queued'].indexOf(this.lemmatizationStatus) === -1) {
-          setTimeout(this.updateStatus, 1000);
-        }
-      },
-      async onRetry() {
-        const response = await api.lemmatizedText_retry(this.text.id);
-        const { data } = response.data;
-        const { completed, tokenCount } = data;
-        this.completed = completed;
-        this.tokenCount = tokenCount;
-        if (this.completed < 100) {
-          setTimeout(this.updateStatus, 1000);
-        }
-      },
-      async onCancel() {
-        const response = await api.lemmatizedText_cancel(this.text.id);
-        const { data } = response.data;
-        const { completed, tokenCount } = data;
-        this.completed = completed;
-        this.tokenCount = tokenCount;
-      },
+    async onRetry() {
+      const response = await api.lemmatizedText_retry(this.text.id);
+      const { data } = response.data;
+      const { completed, tokenCount } = data;
+      this.completed = completed;
+      this.tokenCount = tokenCount;
+      if (this.completed < 100) {
+        setTimeout(this.updateStatus, 1000);
+      }
     },
-  };
+    async onCancel() {
+      const response = await api.lemmatizedText_cancel(this.text.id);
+      const { data } = response.data;
+      const { completed, tokenCount } = data;
+      this.completed = completed;
+      this.tokenCount = tokenCount;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  td {
-    vertical-align: middle;
-  }
-  .progress-bar {
-    transition: width 0.5s ease;
-  }
+td {
+  vertical-align: middle;
+}
+.progress-bar {
+  transition: width 0.5s ease;
+}
 </style>
