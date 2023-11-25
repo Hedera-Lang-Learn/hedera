976d5180a816b14dedc10e90c24d5b5b7f600f2e
ashley-gong <ashley.q.gong@gmail.com>
2023-11-25 11:28:05 -0500

    added total read statistic

--- static/src/js/app/modules/BookmarkList.vue
+++ static/src/js/app/modules/BookmarkList.vue
@@ -1,37 +1,46 @@
 <template>
   <div class="bookmark-list-wrapper">
     <h2>My Bookmarks</h2>
-    <ul class="list-group bookmark-list" v-if="hasBookmarks">
-      <BookmarkItem v-for="(bookmark, index) in bookmarks" :key="index" :bookmark="bookmark" />
-    </ul>
-    <div v-else>
-      You haven't bookmarked any texts yet.
+    <div v-if="hasBookmarks">
+      <ul class="list-group bookmark-list">
+        <BookmarkItem
+          v-for="(bookmark, index) in bookmarks"
+          :key="index"
+          :bookmark="bookmark"
+        />
+      </ul>
+      <h5>Total Read: {{ readCount }}</h5>
     </div>
+    <div v-else>You haven't bookmarked any texts yet.</div>
   </div>
 </template>
 
 <script>
-  import BookmarkItem from './BookmarkItem.vue';
-  import { BOOKMARK_LIST } from '../constants';
+import BookmarkItem from './BookmarkItem.vue';
+import { BOOKMARK_LIST } from '../constants';
 
-  export default {
-    components: { BookmarkItem },
-    created() {
-      this.$store.dispatch(BOOKMARK_LIST);
+export default {
+  components: { BookmarkItem },
+  created() {
+    this.$store.dispatch(BOOKMARK_LIST);
+  },
+  computed: {
+    bookmarks() {
+      return this.$store.state.bookmarks;
     },
-    computed: {
-      bookmarks() {
-        return this.$store.state.bookmarks;
-      },
-      hasBookmarks() {
-        return this.bookmarks.length > 0;
-      },
+    hasBookmarks() {
+      console.log(this.bookmarks);
+      return this.bookmarks.length > 0;
     },
-  };
+    readCount() {
+      return this.bookmarks.filter((b) => b.readStatus == true).length;
+    },
+  },
+};
 </script>
 <style lang="scss">
-  @import "../../../scss/config";
-  .bookmark-list-wrapper {
-    margin-top: 1rem;
-  }
+@import '../../../scss/config';
+.bookmark-list-wrapper {
+  margin-top: 1rem;
+}
 </style>
