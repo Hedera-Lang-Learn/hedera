f0f1be5b81f75521bb872c7dbe8ecfc066ff1d6d
ashley-gong <ashley.q.gong@gmail.com>
2024-06-16 21:36:48 -0700

    more style fixes

--- hedera/serializers.py
+++ hedera/serializers.py
@@ -6,4 +6,4 @@ from vocab_list.models import Folder
 class FolderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Folder
-        fields = ['id', 'name', 'description', 'created_at', 'vocab_lists']
+        fields = ["id", "name", "description", "created_at", "vocab_lists"]
